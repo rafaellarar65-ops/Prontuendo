ac62c01ab8da0bcfc8078065a9662690d2fb82f5
rafaellarar65-ops <rafaellarar65@gmail.com>
2026-03-03 17:45:12 -0300

    refactor templates service to prisma documentTemplate

---
+++ backend/src/templates/dto/update-template.dto.ts
@@ -0,0 +1,5 @@
+import { PartialType } from '@nestjs/swagger';
+
+import { CreateTemplateDto } from './create-template.dto';
+
+export class UpdateTemplateDto extends PartialType(CreateTemplateDto) {}
